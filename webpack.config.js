accc52a42f2bb6d4982cfc9ec7f778e38db0cb43
user01 <zmfl3668@naver.com>
2020-03-16 11:17:36 +0900

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	// enntry file
-	entry: './src/js/main.js',
+	entry: './src/js/index.js',
 	// 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
 	devServer: {
 		port: 9000,
